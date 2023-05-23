d05cbb46eddf6207857e856dd7ae9f397d5c0b6d
Ivan Volosyuk <Ivan.Volosyuk@gmail.com>
2023-05-23 22:15:08 +1000

    Added skeleton PlatformIO esp32dev project.

---
+++ kvs/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(5000);
+  Serial.println("Initialized.");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
