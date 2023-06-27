c5c6430bed9a185f4d9de1b224d0c05cb8d1e8f9
evgeny <evgeny.shatokhin@gmail.com>
2023-06-28 00:14:12 +1000

    Split main.cpp into esp32 and nano33 versions

---
+++ kvs/src/main_esp32.cpp
@@ -0,0 +1,57 @@
+#ifdef ESP32
+
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+#include "storage.hpp"
+
+/* Check if Bluetooth configurations are enabled in the SDK */
+/* If not, then you have to recompile the SDK */
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+
+FlashStorage storage;
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  // delay(5000);
+  Serial.println("Initialized.");
+  storage.Init();
+  auto value = storage.Get("foo");
+  Serial.println(value.c_str());
+
+  /* If no name is given, default 'ESP32' is applied */
+  /* If you want to give your own name to ESP32 Bluetooth device, then */
+  /* specify the name as an argument SerialBT.begin("myESP32Bluetooth*/
+  SerialBT.begin();
+  Serial.println("Bluetooth Started! Ready to pair...");
+}
+
+String readLine() {
+  String line;
+  while (true) {
+    int c = SerialBT.read();
+    if (c == -1) {
+      continue;
+    }
+    if (c == '\n') {
+      return line;
+    }
+    line += char(c);
+  }
+}
+
+void loop() {
+  String s = readLine();
+  Serial.println(s);
+  String toSend = s;
+  toSend.toUpperCase();
+  toSend += '\n';
+  Serial.print(toSend);
+  SerialBT.write((const uint8_t*)toSend.c_str(), toSend.length());
+
+  delay(20);
+}
+#endif  // ESP32
--- kvs/src/main_nano33.cpp
+++ kvs/src/main_nano33.cpp
@@ -1,60 +1,4 @@
-#ifdef ESP32
-
-#include <Arduino.h>
-#include <BluetoothSerial.h>
-#include "storage.hpp"
-
-/* Check if Bluetooth configurations are enabled in the SDK */
-/* If not, then you have to recompile the SDK */
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-
-FlashStorage storage;
-BluetoothSerial SerialBT;
-
-void setup() {
-  Serial.begin(115200);
-  // delay(5000);
-  Serial.println("Initialized.");
-  storage.Init();
-  auto value = storage.Get("foo");
-  Serial.println(value.c_str());
-
-  /* If no name is given, default 'ESP32' is applied */
-  /* If you want to give your own name to ESP32 Bluetooth device, then */
-  /* specify the name as an argument SerialBT.begin("myESP32Bluetooth*/
-  SerialBT.begin();
-  Serial.println("Bluetooth Started! Ready to pair...");
-}
-
-String readLine() {
-  String line;
-  while (true) {
-    int c = SerialBT.read();
-    if (c == -1) {
-      continue;
-    }
-    if (c == '\n') {
-      return line;
-    }
-    line += char(c);
-  }
-}
-
-void loop() {
-  String s = readLine();
-  Serial.println(s);
-  String toSend = s;
-  toSend.toUpperCase();
-  toSend += '\n';
-  Serial.print(toSend);
-  SerialBT.write((const uint8_t*)toSend.c_str(), toSend.length());
-
-  delay(20);
-}
-#else
+#ifndef ESP32
 
 #include <string>
 #include <ArduinoBLE.h>
@@ -155,6 +99,3 @@ void loop() {
   }
 }
 #endif  // !ESP32
-
-#ifdef MBED_CONF_ESP8266_PROVIDE_DEFAULT
-#endif
