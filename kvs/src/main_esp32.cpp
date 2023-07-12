3fb9efb0cb5775f1da799fb313d1099f4282f5a0
evgeny <evgeny.shatokhin@gmail.com>
2023-07-12 23:59:09 +1000

    [esp32] Basic BLE server

--- kvs/src/main_esp32.cpp
+++ kvs/src/main_esp32.cpp
@@ -2,7 +2,7 @@
 
 #include <Arduino.h>
 
-#define BENCHMARK_MODE
+//#define BENCHMARK_MODE
 
 #ifdef BENCHMARK_MODE
 
@@ -124,7 +124,16 @@ void loop() {
 
 #else  // BENCHMARK_MODE
 
-#include <BluetoothSerial.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "64dfd1b8-0c60-46b7-9840-76c1fdcbe011"
+#define CHARACTERISTIC_UUID "a30b08c5-94a3-4678-a026-47dcf3ebec1f"
+
 #include "storage.hpp"
 
 /* Check if Bluetooth configurations are enabled in the SDK */
@@ -133,50 +142,38 @@ void loop() {
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-
 FlashStorage storage;
-BluetoothSerial SerialBT;
-
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   // delay(5000);
   Serial.println("Initialized.");
   storage.Init();
   auto value = storage.Get("foo");
   Serial.println(value.c_str());
 
-  /* If no name is given, default 'ESP32' is applied */
-  /* If you want to give your own name to ESP32 Bluetooth device, then */
-  /* specify the name as an argument SerialBT.begin("myESP32Bluetooth*/
-  SerialBT.begin();
-  Serial.println("Bluetooth Started! Ready to pair...");
-}
+  BLEDevice::init("Key-Value Storage");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
 
-String readLine() {
-  String line;
-  while (true) {
-    int c = SerialBT.read();
-    if (c == -1) {
-      continue;
-    }
-    if (c == '\n') {
-      return line;
-    }
-    line += char(c);
-  }
+  pCharacteristic->setValue("SecretValue");
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
-  String s = readLine();
-  Serial.println(s);
-  String toSend = s;
-  toSend.toUpperCase();
-  toSend += '\n';
-  Serial.print(toSend);
-  SerialBT.write((const uint8_t*)toSend.c_str(), toSend.length());
-
-  delay(20);
+  delay(2000);
 }
 
 #endif  // BENCHMARK_MODE
