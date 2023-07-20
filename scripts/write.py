6ca13e375231808b06b979e8ec49eb80113ca0f2
evgeny <evgeny.shatokhin@gmail.com>
2023-07-20 23:31:07 +1000

    [scripts] Pass the address and the value to write.py

--- scripts/write.py
+++ scripts/write.py
@@ -1,16 +1,16 @@
 import asyncio
+import sys
 from bleak import BleakClient
 
 INPUT_CHARACTERISTIC = "a30b08c5-94a3-4678-a026-47dcf3ebec1f"
 OUTPUT_CHARACTERISTIC = "4c599c12-9cff-4d7b-aea6-b811907f367e"
 
 
-async def main():
-    address = '08EF25D5-5FF3-2617-E116-8F9E689857D1'
+async def main(address, key):
     async with BleakClient(address) as client:
         print(f"Connected: {client.is_connected}")
         print("Sending data...")
-        await client.write_gatt_char(INPUT_CHARACTERISTIC, b"ABC", response=True)
+        await client.write_gatt_char(INPUT_CHARACTERISTIC, key.encode(), response=True)
         print("Data sent")
         await asyncio.sleep(1.0)
         print("Reading response...")
@@ -19,4 +19,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    if len(sys.argv) < 3:
+        print(f"Usage: python3 {sys.argv[0]} ADDRESS VALUE")
+        exit(0)
+    asyncio.run(main(sys.argv[1], sys.argv[2]))
