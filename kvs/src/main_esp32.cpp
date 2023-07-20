93130a457ca664473804e6e2cf0fb8bc4b1b0438
evgeny <evgeny.shatokhin@gmail.com>
2023-07-20 23:50:37 +1000

    [esp32] Minor improvements

--- kvs/src/main_esp32.cpp
+++ kvs/src/main_esp32.cpp
@@ -5,13 +5,6 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
-#define SERVICE_UUID  "64dfd1b8-0c60-46b7-9840-76c1fdcbe011"
-#define INPUT_UUID    "a30b08c5-94a3-4678-a026-47dcf3ebec1f"
-#define OUTPUT_UUID   "4c599c12-9cff-4d7b-aea6-b811907f367e"
-
 #include "storage.hpp"
 
 /* Check if Bluetooth configurations are enabled in the SDK */
@@ -20,7 +13,18 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID  "64dfd1b8-0c60-46b7-9840-76c1fdcbe011"
+#define INPUT_UUID    "a30b08c5-94a3-4678-a026-47dcf3ebec1f"
+#define OUTPUT_UUID   "4c599c12-9cff-4d7b-aea6-b811907f367e"
+
+
 FlashStorage storage;
+
+BLECharacteristic* InputCharacteristic;
+BLECharacteristic* OutputCharacteristic;
 
 class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
@@ -35,21 +39,12 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
 class MyCallbacks : public BLECharacteristicCallbacks
 {
-  BLECharacteristic* _output;
-
-public:
-  MyCallbacks() {}
-
-  MyCallbacks(BLECharacteristic* output) {
-    _output = output;
-  }
-
   void onWrite(BLECharacteristic *pCharacteristic)
   {
     Serial.println("onWrite starts");
     std::string value = pCharacteristic->getValue();
 
-    if (value.length() > 0)
+    if (value.length() > 0 && OutputCharacteristic != nullptr)
     {
       Serial.println("*********");
       Serial.print("New value: ");
@@ -62,7 +57,7 @@ public:
       Serial.println("*********");
 
       std::string output = value + "_secret";
-      _output->setValue(output);
+      OutputCharacteristic->setValue(output);
     }
     Serial.println("onWrite ends");
   }
@@ -79,21 +74,21 @@ void setup() {
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pInputCharacteristic = pService->createCharacteristic(
+  InputCharacteristic = pService->createCharacteristic(
                                          INPUT_UUID,
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  BLECharacteristic *pOutputCharacteristic = pService->createCharacteristic(
+  OutputCharacteristic = pService->createCharacteristic(
                                          OUTPUT_UUID,
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  pOutputCharacteristic->setValue("Value");
-  pInputCharacteristic->setValue("Key");
-  pInputCharacteristic->setCallbacks(new MyCallbacks(pOutputCharacteristic));
+  OutputCharacteristic->setValue("Value");
+  InputCharacteristic->setValue("Key");
+  InputCharacteristic->setCallbacks(new MyCallbacks());
 
   pService->start();
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
