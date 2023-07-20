42a2d6de99b042a31cd0eab8c863a20bbd94384e
evgeny <evgeny.shatokhin@gmail.com>
2023-07-20 23:33:42 +1000

    [scripts] Changed discover.py to scan BLE devices

--- scripts/discover.py
+++ scripts/discover.py
@@ -1,8 +1,20 @@
-import bluetooth
+import asyncio
+from bleak import BleakScanner
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
-print("Found {} devices.".format(len(nearby_devices)))
 
-for addr, name in nearby_devices:
-    print("  {} - {}".format(addr, name))
+async def main():
+    print("scanning for 5 seconds, please wait...")
 
+    devices = await BleakScanner.discover(
+        return_adv=True
+    )
+
+    for d, a in devices.values():
+        print()
+        print(d)
+        print("-" * len(str(d)))
+        print(a)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
