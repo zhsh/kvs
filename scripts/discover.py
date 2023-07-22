3d302388d91ae997c6b75fc126895f9125c6598c
evgeny <evgeny.shatokhin@gmail.com>
2023-07-22 12:51:53 +1000

    [scripts] Auto-discovery of the device. Timeout while waiting for a response.

--- scripts/discover.py
+++ scripts/discover.py
@@ -2,18 +2,28 @@ import asyncio
 from bleak import BleakScanner
 
 
-async def main():
+async def main_all():
     print("scanning for 5 seconds, please wait...")
 
     devices = await BleakScanner.discover(
         return_adv=True
     )
+    
 
     for d, a in devices.values():
         print()
         print(d)
         print("-" * len(str(d)))
         print(a)
+
+async def main():
+    print("scanning for 5 seconds, please wait...")
+
+    device = await BleakScanner.find_device_by_name("Key-Value Storage", timeout = 2)
+    if device:
+        print(device)
+    else:
+        print("Not found")
 
 
 if __name__ == "__main__":
--- scripts/write.py
+++ scripts/write.py
@@ -1,24 +1,35 @@
 import asyncio
 import sys
-from bleak import BleakClient
+from bleak import BleakClient, BleakScanner
 
 INPUT_CHARACTERISTIC = "a30b08c5-94a3-4678-a026-47dcf3ebec1f"
 OUTPUT_CHARACTERISTIC = "4c599c12-9cff-4d7b-aea6-b811907f367e"
 
-def callback(sender, data):
-    print(f"response: {data.decode()}")
 
-async def main(address, key):
-    async with BleakClient(address) as client:
+async def main(key):
+    device = await BleakScanner.find_device_by_name("Key-Value Storage", timeout = 10)
+    if not device:
+        print('Device not found.')
+        exit(0)
+    print('Found the device')
+
+    done = asyncio.Event()
+
+    def callback(sender, data):
+        print(f"response: {data.decode()}")
+        done.set()
+
+    async with BleakClient(device) as client:
         print(f"Connected: {client.is_connected}")
         await client.start_notify(OUTPUT_CHARACTERISTIC, callback)
         print("Sending data...")
         await client.write_gatt_char(INPUT_CHARACTERISTIC, key.encode(), response=True)
         print("Data sent")
+        await asyncio.wait_for(done.wait(), 10)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print(f"Usage: python3 {sys.argv[0]} ADDRESS VALUE")
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 {sys.argv[0]} VALUE")
         exit(0)
-    asyncio.run(main(sys.argv[1], sys.argv[2]))
+    asyncio.run(main(sys.argv[1]))
